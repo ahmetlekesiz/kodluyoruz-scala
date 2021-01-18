948fe6af820fd2167cbfdd96cc0a4c03eb0217a0
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2021-01-18 16:30:02 +0300

    Add street finder solution

--- src/dev/akif/kodluyoruz/streetfinder/Application.scala
+++ src/dev/akif/kodluyoruz/streetfinder/Application.scala
@@ -1,8 +1,6 @@
 package dev.akif.kodluyoruz.streetfinder
 
 import java.io.File
-import scala.io.Source
-import scala.util.{Failure, Success, Try}
 
 /**
  * See CSV file at: https://github.com/makiftutuncu/kodluyoruz-scala/blob/main/data/streets.csv
@@ -10,85 +8,27 @@ import scala.util.{Failure, Success, Try}
  * Original data: https://github.com/life/il-ilce-mahalle-sokak-cadde-sql
  */
 object Application extends CsvLoader with StreetFinder {
-  private val streetsFilePath: String = "data/streets.csv"
 
   def main(args: Array[String]): Unit = {
-    val requestedStreets = Set("ATATÜRK", "İNÖNÜ")
-
-    println(s"Looking for streets matching: ${requestedStreets.mkString("'", "', '", "'")}")
-
-    val startTime = System.currentTimeMillis()
-
-    val allStreets     = loadCsv(new File(streetsFilePath))
-    val matchedStreets = findStreets(allStreets, requestedStreets)
-
-    val elapsedTime = (System.currentTimeMillis() - startTime) / 1000
-
-    println(
-      s"""
-         |Total: ${allStreets.size}
-         |Matched: ${matchedStreets.size}
-         |Took: $elapsedTime second(s)
-         |""".stripMargin)
-
-    matchedStreets foreach println
+    val filePath = "C:\\Users\\Kripton\\Desktop\\Trendyol Data Bootcamp\\kodluyoruz-scala\\data\\streets.csv"
+    val file = new File(filePath)
+    val streetNames = loadCsv(file)
+    val nameSet = Set("ATATÜRK")
+    val result = findStreets(streetNames, nameSet)
+    print(result)
   }
 
-  override def loadCsv(file: File): List[String] =
-    Try(Source.fromFile(file)) match {
-      case Failure(openError) =>
-        fail(s"Cannot open file: $file", openError)
-        List.empty[String]
-
-      case Success(source) =>
-        Try(source.getLines().drop(1).grouped(1024)) match {
-          case Failure(readError) =>
-            source.close()
-            fail(s"Cannot read lines from file: $file", readError)
-            List.empty[String]
-
-          case Success(lineBatches) =>
-            val streets = lineBatches.foldLeft(List.empty[String]) {
-              case (list, batch) =>
-                list ++ extractStreets(batch.toList)
-            }
-            source.close()
-            streets
-        }
-    }
+  override def loadCsv(file: File): List[String] = {
+    val streetNames = io.Source.fromFile(file)
+      .getLines
+      .map(_.split(",")(1))
+      .toList
+      .drop(1)
+    streetNames
+  }
 
   override def findStreets(streets: List[String], names: Set[String]): List[String] = {
-    val streetsToNames =
-      streets.foldLeft(Map.empty[String, Set[String]]) {
-        case (namesMap, street) =>
-          val namesInStreet = street.split(" ").map(_.trim).filter(_.nonEmpty).toSet
-
-          namesMap + (street -> namesInStreet)
-      }
-
-    val matchingStreets = streetsToNames.filter {
-      case (_, namesInStreet) =>
-        namesInStreet.intersect(names).nonEmpty
-    }
-
-    matchingStreets.keys.toList
-  }
-
-  def extractStreets(lines: List[String]): List[String] =
-    lines.foldLeft(List.empty[String]) {
-      case (streets, line) =>
-        line match {
-          case s"$_,'$street',$_" =>
-            streets :+ street
-
-          case _ =>
-            println(s"Cannot extract street name from line: $line")
-            streets
-        }
-    }
-
-  private def fail(message: String, openError: Throwable): Unit = {
-    println(message)
-    openError.printStackTrace()
+    val filteredStreets = streets.filter(e => e.split(" ").filter(p => names.contains(p)).length != 0)
+    filteredStreets
   }
 }
